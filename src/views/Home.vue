aaa7c2c2f2d196e23c8f2f07babf995054ef6952
jiangvicc <binbinbb520@gmail.com>
2022-04-20 16:07:39 +0800

    revise layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col">
           <div class="text-light text-center">
-            <p>Welcome to Taiwan Travel </p>
-            <h2 class="display-4 fw-bold">Taiwan Travel，Travel Go</h2>
+            <p>Welcome to TaipeiTaipei Travel </p>
+            <h2 class="display-4 fw-bold">Taipei Travel，Travel Go</h2>
             <p class="fs-4 mb-5">
               多給自己一點時間享受生活，走出戶外感受大自然
             </p>
@@ -43,7 +43,7 @@
               英丁西久主走放而位荷。皮意弓婆你停可行占您車波新牙雨種也，會房也；尼才花詞。
             </p>
           </div>
-          <div class="card-footer bg-primary rounded-3 rounded-0">
+          <div class="card-footer bg-secondary rounded-3 rounded-0">
             <a href="#" class="text-white ">Let's Go</a>
           </div>
         </div>
@@ -63,7 +63,7 @@
               英丁西久主走放而位荷。皮意弓快發清羊對月也，會房也；尼才花詞。
             </p>
           </div>
-          <div class="card-footer bg-primary rounded-3 rounded-0">
+          <div class="card-footer bg-secondary rounded-3 rounded-0">
             <a href="#" class="text-white ">Let's Go</a>
           </div>
         </div>
@@ -83,7 +83,7 @@
               英丁西久主走放而位荷。尤浪何夏見快發清羊對月也，會房也；尼才花詞。
             </p>
           </div>
-          <div class="card-footer bg-primary rounded-3">
+          <div class="card-footer bg-secondary rounded-3">
             <a href="#" class="text-white ">Let's Go</a>
           </div>
         </div>
@@ -91,23 +91,24 @@
     </div>
   </div>
   <!-- top -->
-  <div class="container">
-    <div class="row row-cols-12 row-cols-md-4">
+  <div class="container mb-4 mb-md-5">
+    <div class="row row-cols-12 row-cols-md-2 row-cols-lg-4">
       <div class="col" v-for="item in scenic_spot" :key="item.ScenicSpotID">
-        <div class="card" style="width: 18rem;">
-          <img :src="item?.Picture?.PictureUrl1" class="card-img-top" alt="...">
+        <div class="card ">
+          <a href="#" class="img-card rounded-top">
+            <img :src="item?.Picture?.PictureUrl1"
+            class="card-imageUrl card-height w-100 object-fit" alt="景點">
+          </a>
           <div class="card-body">
-            <h5 class="card-title">{{item.ScenicSpotName}}</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 class="card-title">{{ item.ScenicSpotName }}</h5>
+            <p class="card-text"><i class="bi bi-clock"></i> {{ item.OpenTime }}</p>
           </div>
-          <ul class="list-group list-group-flush">
-            <li class="list-group-item">An item</li>
-            <li class="list-group-item">A second item</li>
-            <li class="list-group-item">A third item</li>
+          <ul class="list-group list-group-flush d-flex justify-content-center" style="height:80px">
+            <li class="list-group-item"><i class="bi bi-geo-alt"></i> {{ item.Address }}</li>
           </ul>
-          <div class="card-body">
-            <a href="#" class="card-link">Card link</a>
-            <a href="#" class="card-link">Another link</a>
+          <div class="card-body d-flex d-grid gap-2 mx-auto ">
+            <a href="#" class="btn btn-outline-secondary card-link hover-color">查看詳情</a>
+            <a href="#" class="btn btn-secondary card-link text-white">立即出發</a>
           </div>
         </div>
       </div>
@@ -129,15 +130,32 @@ export default {
   methods: {
     getData() {
       this.isLoading = true;
-      const url = 'https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?%24top=8&%24format=JSON';
+      // const url = 'https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?%24format=JSON';
+      const url = 'https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/Taipei?%24top=8&%24format=JSON';
       this.$http.get(url, {
         headers: getAuthorizationHeader(),
       })
         .then((response) => {
+          console.log(response);
           this.scenic_spot = response.data;
           console.log(this.scenic_spot);
           this.isLoading = false;
         });
+    },
+  },
+  computed: {
+    // 提取出 Class1 的分類們
+    filterCategory() {
+      // 提取出 category
+      const category = this.scenic_spot.map((item) => item.Class1);
+      // 過濾出重複的元素，重複的會被剔除掉
+      return [...new Set(category)];
+    },
+    filterCategoryClass() {
+      // 提取出 category
+      const category = this.scenic_spot.filter((item) => item.Class1 === '自然風景類');
+      // 過濾出重複的元素，重複的會被剔除掉
+      return [...new Set(category)];
     },
   },
   mounted() {
@@ -153,6 +171,19 @@ export default {
   background-position: bottom center;
   background-size: cover;
 }
+
+.card-height{
+  height: 200px;
+}
+
+.hover-color:hover{
+  color: white;
+}
+
+.object-fit{
+  object-fit:cover;
+}
+
 .img-card {
   width: 100%;
   overflow: hidden;
--- src/views/Location.vue
+++ src/views/Location.vue
@@ -1,3 +1,114 @@
 <template>
-  景點
+    <div class="container">
+      <div class="row">
+        <div class="div">
+          <select class="citySelect" >
+            <option value="city" v-for="item in filterCategory" :key="item+1">{{item}}</option>
+          </select>
+          <input type="button" class="send" value="搜尋">
+          <ul class="list">
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <div class="div" v-for="(item, index) in filterCategory" :key="index">
+          {{item}}
+        </div>
+      </div>
+      <div class="row">
+        <div class="div" v-for="(item) in filterCategoryClass" :key="item.ScenicSpotID">
+            {{ item.ScenicSpotName }}
+        </div>
+      </div>
+  </div>
+    <div class="container mb-4 mb-md-5">
+    <div class="row row-cols-12 row-cols-md-2 row-cols-lg-4">
+      <div class="col" v-for="item in scenic_spot" :key="item.ScenicSpotID">
+        <div class="card ">
+          <a href="#" class="img-card rounded-top">
+            <img :src="item?.Picture?.PictureUrl1" class="card-imageUrl card-height w-100 object-fit" alt="景點">
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">{{ item.ScenicSpotName }}</h5>
+            <p class="card-text"><i class="bi bi-clock"></i> {{ item.OpenTime }}</p>
+          </div>
+          <ul class="list-group list-group-flush d-flex justify-content-center" style="height:80px">
+            <li class="list-group-item"><i class="bi bi-geo-alt"></i> {{ item.Address }}</li>
+          </ul>
+          <div class="card-body d-flex d-grid gap-2 mx-auto ">
+            <a href="#" class="btn btn-outline-secondary card-link hover-color">查看詳情</a>
+            <a href="#" class="btn btn-secondary card-link text-white">立即出發</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+import getAuthorizationHeader from '../methods/getAuthorizationHeader';
+
+export default {
+  data() {
+    return {
+      city: [],
+      scenic_spot: [],
+      isLoading: false,
+    };
+  },
+  methods: {
+    getData() {
+      this.isLoading = true;
+      // const url = 'https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?%24format=JSON';
+      const url = 'https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/Taipei?%24format=JSON';
+      this.$http.get(url, {
+        headers: getAuthorizationHeader(),
+      })
+        .then((response) => {
+          console.log(response);
+          this.scenic_spot = response.data;
+          console.log(this.scenic_spot);
+          this.isLoading = false;
+        });
+    },
+    getFilter() {
+      this.isLoading = true;
+      // const url = 'https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?%24format=JSON';
+      const url = 'https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot/Taipei?%24format=JSON';
+      this.$http.get(url, {
+        headers: getAuthorizationHeader(),
+      })
+        .then((response) => {
+          console.log(response);
+          this.scenic_spot = response.data;
+          console.log(this.scenic_spot);
+          this.isLoading = false;
+        });
+    },
+  },
+  computed: {
+    // 提取出 Class1 的分類們
+    filterCategory() {
+      // 提取出 category
+      const category = this.scenic_spot.map((item) => item.Class1);
+      // 過濾出重複的元素，重複的會被剔除掉
+      return [...new Set(category)];
+    },
+    filterCategoryClass() {
+      // 提取出 category
+      const category = this.scenic_spot.filter((item) => item.Class1 === '自然風景類');
+      // 過濾出重複的元素，重複的會被剔除掉
+      return [...new Set(category)];
+    },
+    filterCategoryCity() {
+      // 提取出 category
+      const city = this.scenic_spot.map((item) => item.City);
+      // 過濾出重複的元素，重複的會被剔除掉
+      return [...new Set(city)];
+    },
+  },
+  mounted() {
+    this.getData();
+  },
+};
+</script>
